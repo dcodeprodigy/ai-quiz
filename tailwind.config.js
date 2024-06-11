d0e539a78975efab8a14c774a6a2ec0073fc2983
Joseph Nwodo <128279131+dcodeprodigy@users.noreply.github.com>
2024-06-11 09:00:35 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./mcq_generation/**/*.{html,js}", "./api/**/*.{html,js}", "./node_practice/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
