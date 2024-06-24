d0abd49bf57117886bf5d95d04cc6689a932e9b6
Joseph Nwodo <128279131+dcodeprodigy@users.noreply.github.com>
2024-06-24 14:13:33 +0100

    fixed post url

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ async function runChecks(quizForm) {
 
 
     try {
-      await axios.post('http://0.0.0.0:5500/genQuestions', formData, {
+      await axios.post('/genQuestions', formData, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         }
