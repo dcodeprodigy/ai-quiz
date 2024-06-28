3cbe38a661f4f97c762a35215ae547d649ee3ebd
Joseph Nwodo <128279131+dcodeprodigy@users.noreply.github.com>
2024-06-28 06:18:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } = require('@googl
 // Middleware to parse JSON bodies
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname)));
 
 const safetySetting = [
   {
